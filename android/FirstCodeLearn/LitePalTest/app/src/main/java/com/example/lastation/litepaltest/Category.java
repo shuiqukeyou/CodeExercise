498cc297f04e0b83197d248bc0b44019ce78a99a
ShuiQu <shuiqukeyou@gmail.com>
2017-02-16 17:18:30 +0800

    进度：第一行代码第六章结束

---
+++ android/FirstCodeLearn/LitePalTest/app/src/main/java/com/example/lastation/litepaltest/Category.java
@@ -0,0 +1,35 @@
+package com.example.lastation.litepaltest;
+
+/**
+ * Created by lastation on 2017/2/16.
+ */
+
+public class Category {
+    private int id;
+    private String categoryName;
+    private int categoryCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    public int getCategoryCode() {
+        return categoryCode;
+    }
+
+    public void setCategoryCode(int categoryCode) {
+        this.categoryCode = categoryCode;
+    }
+}
