de2604fdc47e7091ff73348d1593cb32ab09ddf3
ShuiQu <shuiqukeyou@gmail.com>
2017-03-11 13:46:57 +0800

    DAO层接口

---
+++ java/bookStore/src/com/lastation/exercise/bookSrore/user/dao/dao/UserDao.java
@@ -0,0 +1,33 @@
+package com.lastation.exercise.bookSrore.user.dao.dao;
+
+import java.util.List;
+
+import com.lastation.exercise.bookSrore.user.vo.UserQueryValueObject;
+import com.lastation.exercise.bookSrore.user.vo.UserValueObject;
+
+public interface UserDao {
+	/**
+	 * 创建新用户
+	 * @param user 将要被创建的用户对象
+	 * @return 创建成功则返回true，否则返回false
+	 */
+	boolean create(UserValueObject user);
+	
+	/**
+	 * 删除用户
+	 * @param user 将要被删除的用户的uuid
+	 * @return 删除成功则返回true，否则返回false
+	 */
+	boolean delete(String uuid);
+	
+	/**
+	 * 更新用户信息
+	 * @param user 将要更新的用户对象
+	 * @return 更新成功则返回true，否则返回false
+	 */
+	boolean update(UserValueObject user);
+	
+	UserValueObject findUser(String uuid);
+	List<UserValueObject> findAll();
+	List<UserValueObject> findByCondition(UserQueryValueObject uqvo);
+}
