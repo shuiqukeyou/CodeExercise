d2b3ddc4a8049a749a39191d26a921dbc4d83203
shuiqukeyou <shuiqukeyou@gmail.com>
2019-04-16 14:53:08 +0800

    数据可视化——BTC

--- python/python crash course/dataVisualization/hn_submission.py
+++ python/python crash course/dataVisualization/hn_submission.py
@@ -4,7 +4,7 @@ from operator import itemgetter
 
 url = "https://hacker-news.firebaseio.com/v0/topstories.json"
 r = requests.get(url)
-print("响应码：",r.status_code)
+print("响应码：", r.status_code)
 
 submission_ids = r.json()
 submission_dicts = []
@@ -15,9 +15,9 @@ for submission_id in submission_ids[:30]:
     requests_dict = submission_r.json()
 
     submission_dict = {
-        "title":requests_dict["title"],
-        "link":"http://news.ycombinator.com/item?id=" + str(submission_id),
-        "comments":requests_dict.get("descendants", 0)
+        "title": requests_dict["title"],
+        "link": "http://news.ycombinator.com/item?id=" + str(submission_id),
+        "comments": requests_dict.get("descendants", 0)
     }
     submission_dicts.append(submission_dict)
 
@@ -25,6 +25,6 @@ for submission_id in submission_ids[:30]:
 submission_dicts = sorted(submission_dicts, key=itemgetter("comments"), reverse=True)
 
 for submission_dict in submission_dicts:
-    print("\nTitle:",submission_dict["title"])
-    print("Discussion link:",submission_dict["link"])
-    print("Comments:",submission_dict["comments"])
+    print("\nTitle:", submission_dict["title"])
+    print("Discussion link:", submission_dict["link"])
+    print("Comments:", submission_dict["comments"])
